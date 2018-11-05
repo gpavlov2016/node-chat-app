48eaf3d1775a7802e685a9f1033f5444ab90eb7f
Guy Pavlov <guy.pavlov84@gmail.com>
2018-11-05 14:45:08 -0500

    Emit new message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Guy',
-    text: 'sample'
-  });
 });
 
 socket.on('disconnect', function () {
